1c18698f28a874d64984d4847553a1d95ebba516
Herson Oliveira <oliveira.herson@gmail.com>
2019-05-25 11:35:38 +0200

    Update methods

--- src/main/scala/com/ninetynine/problems/lists/P01.scala
+++ src/main/scala/com/ninetynine/problems/lists/P01.scala
@@ -5,5 +5,6 @@ object P01 {
   def last[A](list: List[A]): A = list match {
     case last :: Nil => last
     case _ :: tail => last(tail)
+    case _ => throw new NoSuchElementException
   }
 }
--- src/main/scala/com/ninetynine/problems/lists/P02.scala
+++ src/main/scala/com/ninetynine/problems/lists/P02.scala
@@ -3,7 +3,8 @@ package com.ninetynine.problems.lists
 object P02 {
 
   def penultimate[A](list: List[A]): A = list match {
-    case x :: y :: Nil => x
+    case x :: _ :: Nil => x
     case _ :: tail => penultimate(tail)
+    case _ => throw new NoSuchElementException
   }
 }

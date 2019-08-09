9f6c045b4177a543e033c787d2b873c630ae2c79
Herson Oliveira <oliveira.herson@gmail.com>
2019-08-09 18:35:09 +0200

    Add solution 16

--- src/main/scala/com/ninetynine/problems/lists/P16.scala
+++ src/main/scala/com/ninetynine/problems/lists/P16.scala
@@ -2,6 +2,10 @@ package com.ninetynine.problems.lists
 
 object P16 {
 
-  def drop[A](pos: Int, list: List[A]): List[A] = list diff List(list(pos - 1))
+  def drop[A](pos: Int, list: List[A]): List[A] = {
+    list filterNot {
+      x => ((list indexOf (x)) + 1) % pos == 0
+    }
+  }
 
 }
--- src/test/scala/com/ninetynine/problems/lists/P16Test.scala
+++ src/test/scala/com/ninetynine/problems/lists/P16Test.scala
@@ -6,9 +6,14 @@ class P16Test extends FunSuite with Matchers {
 
   val inputList = List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)
 
-  test("drop()") {
+  test("drop() pos = 3") {
     val expected = List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k)
     P16.drop(3, inputList) shouldBe (expected)
+  }
+
+  test("drop() pos = 2") {
+    val expected = List('a, 'c, 'e, 'g, 'i, 'k)
+    P16.drop(2, inputList) shouldBe (expected)
   }
 
 }
